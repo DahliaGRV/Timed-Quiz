fcdd22169ca935c0bbcb0ac49f3eee148e031892
Dahlia Graves <dahlialolagraves@gmail.com>
2022-04-03 22:57:05 -0700

    added questions...list click not working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var upTime = document. querySelector("#upTime");
 var highScore =document.querySelector("#highscore");
 var headerEl = document.querySelector("header");
-var marriageQuestions = document.querySelector("section")
+var questions = document.querySelector("section")
 var timer;
 var timerLeft = 200;
 var score = localStorage.getItem("score") || 0;
@@ -45,15 +45,7 @@ var isPlaying = false;
 
     function firstQuestion() {
     headerEl.setAttribute("style","display:none");
-    startBtn.setAttribute("style","display:none")
-    // var state = headerEl.getAttribute("data-state");
-    // var state1 = startBtn.getAttribute("data-state");
-    // if (state1 === "visible") {
-    //     startBtn.dataset.state = "hidden"
-    // };
-    // if (state === "visible") {
-    // headerEl.dataset.state = "hidden"
-    // };
+    startBtn.setAttribute("style","display:none");
     // Create Question
     var marriage = document.createElement("p");
     // Create ordered list element
@@ -71,8 +63,8 @@ var isPlaying = false;
     li4.textContent = "Nine"
 
     // Append ordered list 
-    marriageQuestions.appendChild(marriage);
-    marriageQuestions.appendChild(listEl);
+    questions.appendChild(marriage);
+    questions.appendChild(listEl);
     listEl.appendChild(li1);
     listEl.appendChild(li2);
     listEl.appendChild(li3);
@@ -86,19 +78,131 @@ var isPlaying = false;
 
     listEl.addEventListener("click",function(event){
         var element = event.target;
-        if (element.matches(li3)) {
+        if (element.matches("li3")) {
             var correct = document.createElement("p");
-            correct.textContent = "Correct!!"
+            correct.textContent = "Correct!!";
             marriageQuestions.appendChild(correct);
-            correct.setAttribute("style","color:darkgreen; border-top: black dotted 2px;")
+            correct.setAttribute("style","color:darkgreen; border-top: black dotted 2px;");
+            console.log("Hello!");
+            // store score in localStorage based on time on clock when clicked?
             secondQuestion();
-    } else if (element.matches(li1||l12||l14)) {
+    } else if (element.matches("li1"||"li2"||"li4")) {
         // Placeholder alert
-        alert("Wrong Answer!")
-// function secondQuestion () {}
+        var wrong = document.createElement("p");
+        wrong.textContent = "Wrong!!";
+        marriageQuestions.appendChild(wrong);
+        wrong.setAttribute("style","color:darkgreen; border-top: black dotted 2px;")
+        // Put in time deduction function here for wrong answer
+
     }});
 };
+    function secondQuestion() {
+        headerEl.setAttribute("style","display:none");
+        startBtn.setAttribute("style","display:none");
+        // Create Question
+        var quarryMine = document.createElement("p");
+        // Create ordered list element
+        var listEl = document.createElement("ol");
+        // Create ordered list items
+        var li1 = document.createElement("li");
+        var li2 = document.createElement("li");
+        var li3 = document.createElement("li");
+        var li4 = document.createElement("li");
+    
+        quarryMine.textContent = "What do you recieve at the end of the quarry mine?"
+        li1.textContent = "Prismatic Shard"
+        li2.textContent = "Void Pendant"
+        li3.textContent = "Diamond"
+        li4.textContent = "Golden Scythe"
+    
+        // Append ordered list 
+        questions.appendChild(quarryMine);
+        questions.appendChild(listEl);
+        listEl.appendChild(li1);
+        listEl.appendChild(li2);
+        listEl.appendChild(li3);
+        listEl.appendChild(li4);
+        quarryMine.setAttribute("style","margin-top:15%;color:white; font-weight:bold; display:flex; flex-wrap:wrap; justify-content:center; text-align:center background-color:pink; padding: 5px;");
+        listEl.setAttribute("style"," display:block;flex-direction:column;justify-content:center;max-width:10%;margin-left:43%;text-align:center; lightgrey 1px solid;");
+        li1.setAttribute("style","background-color:purple;color:white;padding:2%; margin: 2%;border:white 2px solid;");
+        li2.setAttribute("style","background-color:purple;color:white;padding:2%; margin: 2%;border:white 2px solid;");
+        li3.setAttribute("style","background-color:purple;color:white;padding:2%; margin: 2%;border:white 2px solid;");
+        li4.setAttribute("style","background-color:purple;color:white;padding:2%; margin: 2%;border:white 2px solid;");
 
+        listEl.addEventListener("click",function(event){
+            var element = event.target;
+            if (element.matches("li4")) {
+                var correct = document.createElement("p");
+                correct.textContent = "Correct!!";
+                marriageQuestions.appendChild(correct);
+                correct.setAttribute("style","color:darkgreen; border-top: black dotted 2px;");
+                console.log("Hello!");
+                // store score in localStorage based on time on clock when clicked?
+                thirdQuestion();
+        } else if (element.matches("li1"||"li2"||"li3")) {
+            // Placeholder alert
+            var wrong = document.createElement("p");
+            wrong.textContent = "Wrong!!";
+            questions.appendChild(wrong);
+            wrong.setAttribute("style","color:darkgreen; border-top: black dotted 2px;")
+            // Put in time deduction function here for wrong answer
+    
+        }});
+
+    };
+    function thirdQuestion() {
+        headerEl.setAttribute("style","display:none");
+        startBtn.setAttribute("style","display:none");
+        // Create Question
+        var paternity = document.createElement("p");
+        // Create ordered list element
+        var listEl = document.createElement("ol");
+        // Create ordered list items
+        var li1 = document.createElement("li");
+        var li2 = document.createElement("li");
+        var li3 = document.createElement("li");
+        var li4 = document.createElement("li");
+    
+        paternity.textContent = "Which NPC has questionable paternity?"
+        li1.textContent = "Abigail"
+        li2.textContent = "Vincent"
+        li3.textContent = "Emily"
+        li4.textContent = "Maru"
+    
+        // Append ordered list 
+        questions.appendChild(paternity);
+        questions.appendChild(listEl);
+        listEl.appendChild(li1);
+        listEl.appendChild(li2);
+        listEl.appendChild(li3);
+        listEl.appendChild(li4);
+        paternity.setAttribute("style","margin-top:15%;color:white; font-weight:bold; display:flex; flex-wrap:wrap; justify-content:center; text-align:center background-color:pink; padding: 5px;");
+        listEl.setAttribute("style"," display:block;flex-direction:column;justify-content:center;max-width:10%;margin-left:43%;text-align:center; lightgrey 1px solid;");
+        li1.setAttribute("style","background-color:purple;color:white;padding:2%; margin: 2%;border:white 2px solid;");
+        li2.setAttribute("style","background-color:purple;color:white;padding:2%; margin: 2%;border:white 2px solid;");
+        li3.setAttribute("style","background-color:purple;color:white;padding:2%; margin: 2%;border:white 2px solid;");
+        li4.setAttribute("style","background-color:purple;color:white;padding:2%; margin: 2%;border:white 2px solid;");
+
+        listEl.addEventListener("click",function(event){
+            var element = event.target;
+            if (element.matches("li1")) {
+                var correct = document.createElement("p");
+                correct.textContent = "Correct!!";
+                questions.appendChild(correct);
+                correct.setAttribute("style","color:darkgreen; border-top: black dotted 2px;");
+                console.log("Hello!");
+                // store score in localStorage based on time on clock when clicked?
+                fourthQuestion();
+        } else if (element.matches("li2"||"li3"||"li4")) {
+            // Placeholder alert
+            var wrong = document.createElement("p");
+            wrong.textContent = "Wrong!!";
+            questions.appendChild(wrong);
+            wrong.setAttribute("style","color:darkgreen; border-top: black dotted 2px;")
+            // Put in time deduction function here for wrong answer
+    
+        }});
+    };
 // TODO: Each Question Page
     // Has 'view highscores' link in every top left corner HTML
     // Change the h1,h2, h3 tags to be questions or to be hidden 
